e0c411370a1243b115972c46c4ca8929d18a1fb3
alekseyb <aberkunsky@gmail.com>
2018-06-26 11:02:45 +0300

    Initial commit

---
+++ src/test/java/com/bravemamoru/testprojectunibot/TestprojectunibotApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bravemamoru.testprojectunibot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestprojectunibotApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
